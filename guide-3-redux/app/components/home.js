f63fc799516c857ab40c0714b36ce2d5f277889b
Ahmed <ahmed.ziani.90@gmail.com>
2017-04-11 17:01:33 +0200

    Update home.js

--- guide-3-redux/app/components/home.js
+++ guide-3-redux/app/components/home.js
@@ -9,7 +9,7 @@ const Home = React.createClass({
           While the <a href="#">CSS-Tricks article</a> for
           this guide covers an explanation of <strong>Redux</strong>, there
           are still many implementation details in this code that the article
-          doesn't cover. For a better understanding of those details, see
+          doesn/'t cover. For a better understanding of those details, see
           the <a href="https://github.com/bradwestfall/CSS-Tricks-React-Series">Github documentation</a> for
           this guide.
         </p>
